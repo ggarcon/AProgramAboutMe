ac1dd8794e68651a7927c68cd891d00b12e1e829
Gaetan Garcon <gaetangarcon@Gaetans-MacBook-Air.local>
2017-10-02 19:15:05 -0400

    Initial commit

---
+++ src/AProgramForMe.java
@@ -0,0 +1,8 @@
+public class AProgramForMe {
+    public static void main( String[] args )
+    {
+        System.out.println("Hello!");
+        System.out.println("My name is Gaetan Garcon");
+        System.out.println("I am 20 years old");
+    }
+    }
